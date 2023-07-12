9ea0e16145be607a86696c0ca2ddb25fbb24c5e9
i088omor <138637121+i088omor@users.noreply.github.com>
2023-07-12 11:04:13 +0900

    a.php

--- a.php
+++ a.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
-<?php 
-  foreach(file('/usr/share/dict/linux.words') as $line){
-    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-  } 
+<?php
+  foreach(file('/usr/share/dict/words') as $line){
+// strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
+          if(strpos($line, 'abc') !== false) echo "$line";
+  }
 ?>
